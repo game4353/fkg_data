a643e16efe4d8e7c59b8f50e6884e2156ad038b8
fkg4353 <behepe.cc@gmail.com>
2022-04-20 13:58:14 +0800

    add element

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 
 function main() {
     Charas.init()
-    let arr = [["No", "ID", "icon", "evo", "rare", "name", "skillDesc", "abilityDesc"]]
+    let arr = [['No', 'ID', 'icon', 'evo', 'rare', 'prop', 'name', 'skillDesc', 'abilityDesc']]
     for(let chara of Charas.getKnight()){
         arr.push(arr[0].map(h => chara[h]))
     }
     const table = new Table(arr)
-    table.setWidth([30, 80, 80, 30, 30, 200, 400, 500])
-    $("#outer").empty().append(table.table)
+    table.setWidth([30, 80, 80, 30, 30, 30, 200, 400, 500])
+    $('#outer').empty().append(table.table)
 }
